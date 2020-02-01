bfb061da804f4c25e621a1aee2cc94699ff6a99d
spo <spo@center.mddm>
2020-02-01 17:54:13 +0300

    commit

--- src/pages/AdminPage/components/ImagesView.js
+++ src/pages/AdminPage/components/ImagesView.js
@@ -2,12 +2,14 @@ import React from "react";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 
-const ImagesView = props => {
+const ImagesView = ({ url: urls }) => {
   return (
-    <GridList cellHeight={160} cols={3}>
-      <GridListTile key={tile.img} cols={tile.cols || 1}>
-        <img src={tile.img} alt={tile.title} />
-      </GridListTile>
+    <GridList cellHeight={160} cols={40}>
+      {urls.slice(0, 200).map(url => (
+        <GridListTile key={url.url} cols={5}>
+          <img src={url.url} alt={url.url} />
+        </GridListTile>
+      ))}
     </GridList>
   );
 };
--- src/pages/AdminPage/containers/ImagesContainer.js
+++ src/pages/AdminPage/containers/ImagesContainer.js
@@ -3,35 +3,31 @@ import ImagesView from "../components/ImagesView";
 
 class ImagesContainer extends React.Component {
   state = {
-    tileData: [
-      {
-        img: image,
-        title: "Image",
-        author: "author",
-        cols: 2
-      },
-    ]
+    url: []
   };
+
+  componentDidMount() {
+    this.getImgs();
+  }
+
   getImgs() {
-    fetch("https://jsonplaceholder.typicode.com/photos")
-    .then(response => response.json())
-    .then(photos => {
-      const mappedPhotos = photos.map(photos => ({
-        url: photos.url,
-        
-      }))
-   }
-}
+    fetch(
+      "https://pixabay.com/api?key=15107156-da183c9b8b82bda79cb3047a5&colors=grey&per_page=100"
+    )
+      .then(response => response.json())
+      .then(photos => {
+        const mappedPhotos = photos.hits.map(photos => ({
+          url: photos.largeImageURL
+        }));
+        this.setState({ url: mappedPhotos });
+      });
+  }
+
   render() {
     const props = {
-      tileData: this.state.tileData
+      url: this.state.url
     };
-    return (
-      <>
-        <></>
-        <ImagesView {...props} />
-      </>
-    );
+    return <ImagesView {...props} />;
   }
 }
 
